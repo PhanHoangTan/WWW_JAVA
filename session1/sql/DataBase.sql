dbfff275d62debe8b382a5e8e338ec598e28be69
MSI\Admin <phanhoangtan.job@gmail.com>
2024-08-24 20:14:34 +0700

    up

---
+++ session1/sql/DataBase.sql
@@ -0,0 +1,21 @@
+﻿-- 1. Tạo cơ sở dữ liệu Login
+--CREATE DATABASE Login;
+
+-- 2. Chọn cơ sở dữ liệu Login để sử dụng
+USE Login;
+
+-- 3. Tạo bảng users
+CREATE TABLE users (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+-- 4. Thêm dữ liệu mẫu
+INSERT INTO users (username, password) VALUES ('ty', '123');
+INSERT INTO users (username, password) VALUES ('admin', 'adminpass');
+
+-- 5. Kiểm tra dữ liệu
+SELECT * FROM users;
+
+SELECT * FROM users WHERE username = 'ty' AND password = 123
